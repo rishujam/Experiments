594ff21c9a3c98df505cacdfcb704c2f4440bd58
Sudhanshu kumar <rishuparashar7@gmail.com>
2023-09-07 22:39:02 +0530

    pdf viewer working

--- app/src/main/java/com/example/experiments/pdf/PdfActivity.kt
+++ app/src/main/java/com/example/experiments/pdf/PdfActivity.kt
@@ -15,15 +15,7 @@ class PdfActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPdfBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val data = mutableListOf<PdfUiModel>()
-        val d1 = ContextCompat.getDrawable(this, R.drawable.sample)?.toBitmap()
-        val d2 = ContextCompat.getDrawable(this, R.drawable.sample)?.toBitmap()
-        if(d1 != null && d2 != null) {
-            data.add(PdfUiModel(d1, 1, 2))
-            data.add(PdfUiModel(d2, 2, 2))
-            binding.pdfView.setData(data)
-        }
+        binding.pdfView.setData("/data/data/com.example.experiments/files/sample_pdf.pdf")
     }
 
 
--- app/src/main/java/com/example/experiments/pdf/PdfView.kt
+++ app/src/main/java/com/example/experiments/pdf/PdfView.kt
@@ -1,19 +1,29 @@
 package com.example.experiments.pdf
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
+import android.os.ParcelFileDescriptor
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.example.experiments.R
+import com.example.experiments.databinding.PdfItemViewBinding
 import com.example.experiments.databinding.PdfViewBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import java.io.File
+import java.net.URI
 
 /*
  * Created by Sudhanshu Kumar on 06/09/23.
@@ -24,36 +34,58 @@ class PdfView @JvmOverloads constructor(
     attrs: AttributeSet? = null
 ) : ConstraintLayout(context, attrs) {
 
-    private var binding: PdfViewBinding
     private lateinit var pdfAdapter: PDFAdapter
     private var noOfPages: Int? = null
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+    private var pageText: TextView
+    private var pageCard: CardView
 
     init {
-        View.inflate(context, R.layout.pdf_view, this)
-        binding = PdfViewBinding.inflate(LayoutInflater.from(context))
+        removeAllViews()
+        LayoutInflater.from(context).inflate(R.layout.pdf_view, this, true)
+        pageText = findViewById(R.id.tvPageNo)
+        pageCard = findViewById(R.id.cdPages)
         setupRecyclerView()
     }
 
     private fun setupRecyclerView() {
         pdfAdapter = PDFAdapter()
-        binding.pdfRv.apply {
+        val rv = findViewById<RecyclerView>(R.id.pdfRv)
+        rv.apply {
             adapter = pdfAdapter
             layoutManager = LinearLayoutManager(context)
             setOnScrollChangeListener { _, _, _, _, _ ->
                 val currPage = (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition() + 1
                 noOfPages?.let {
-                    binding.tvPageNo.text = "$currPage / $it"
-                    binding.cdPages.showPagesViewForSomeSeconds(coroutineScope, context)
+                    pageText.text = "$currPage / $it"
+                    pageCard.showPagesViewForSomeSeconds(coroutineScope, context)
                 }
             }
         }
     }
 
-    fun setData(list: List<PdfUiModel>) {
-        pdfAdapter.differ.submitList(list)
+    fun setData(uri: String) {
+        val list = generateBitmaps(uri)
         noOfPages = list.size
-        binding.cdPages.showPagesViewForSomeSeconds(coroutineScope, context)
+        pdfAdapter.differ.submitList(list)
+        pageCard.showPagesViewForSomeSeconds(coroutineScope, context)
+    }
+
+    private fun generateBitmaps(uri: String): List<PdfUiModel> {
+        val output = mutableListOf<PdfUiModel>()
+        val input = ParcelFileDescriptor.open(File(uri), ParcelFileDescriptor.MODE_READ_ONLY)
+        val renderer = PdfRenderer(input)
+        val pageCount = renderer.pageCount
+        for(i in 0 until pageCount) {
+            val page = renderer.openPage(i)
+            val bitmap = Bitmap.createBitmap(page.width, page.height, Bitmap.Config.ARGB_8888)
+            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+            val model = PdfUiModel(bitmap, i + 1, pageCount)
+            output.add(model)
+            page.close()
+        }
+        renderer.close()
+        return output
     }
 
 }
